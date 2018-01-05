11e3b0884cdedfd699f2be2991d32e9068b349e0
fallingstar19 <aquas14@hotmail.com>
2018-01-05 14:24:12 +0000

    git commit -am 'updated commands.txt' git commit -am 'added alina.txt'

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const fs = require('fs');
 
 var commandsList = fs.readFileSync('./commands.txt', 'utf8');
 
+var alinaDefinition = fs.readFileSync('./alina.txt', 'utf8');
+
 
 const fortunes = [
   ':8ball: | Yes',
@@ -33,23 +35,26 @@ bot.on('message', (message) => {
   const args = message.content.substring(prefix.length).split(' ');
   switch (args[0].toLowerCase()) {
     case 'impossible':
-      message.channel.sendMessage('I believe you meant *improbable*');
+      message.channel.send('I believe you meant *improbable*');
       break;
     case 'advice':
-      message.channel.sendMessage("I hope you weren't looking to me to be the voice of reason. I keep to a strict diet of ill-advised enthusiasm and heartfelt regret.");
+      message.channel.send("I hope you weren't looking to me to be the voice of reason. I keep to a strict diet of ill-advised enthusiasm and heartfelt regret.");
       break;
     case 'name':
-      message.channel.sendMessage('Nikolai. But I’ve also been known to answer to ‘sweetheart’ or ‘handsome’.');
+      message.channel.send('Nikolai. But I’ve also been known to answer to ‘sweetheart’ or ‘handsome’.');
       break;
     case '8ball':
-      if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-      else message.channel.sendMessage("Can't read nonsense");
+      if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+      else message.channel.send("Can't read nonsense");
       break;
     case 'help':
       message.channel.send(commandsList);
       break;
     case 'delicacy':
-      message.channel.sendMessage(':cookie: | but ***you*** are the true delicacy');
+      message.channel.send(':cookie: | but ***you*** are the true delicacy');
+      break;
+    case 'alina':
+      message.channel.send(alinaDefinition);
       break;
    
   }
